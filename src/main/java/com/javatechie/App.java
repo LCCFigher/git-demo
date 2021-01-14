a877c3783a69a24663a20669f02d6ccd9b78329e
NawazAliFighter <chaudharybsc13@gmail.com>
2021-01-14 22:10:23 +0530

    added git- demo project to local repo

---
+++ src/main/java/com/javatechie/App.java
@@ -0,0 +1,15 @@
+package com.javatechie;
+
+public class App {
+
+	public void addData(String data)
+	{
+		System.out.println("data ="+ data);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello World");
+	}
+
+}
